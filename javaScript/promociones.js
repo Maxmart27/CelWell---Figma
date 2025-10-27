87c604679cb67e5daaf04e4f6e7d520b85d49426
Maxmart27 <Maximmart007@gmail.com>
2025-10-27 16:36:49 -0300

    Agregado de boton calcular, FALTA SOLO FUNCIONALIDAD DE CALCULADORA

--- javaScript/promociones.js
+++ javaScript/promociones.js
@@ -1,3 +1,60 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
     
+    //FUNCIONAMIENTO DE LOS CHECKBOX Y CONTROLES DE CANTIDAD
+    const products = document.querySelectorAll('.product-item');
+    const calcButton = document.querySelector('.calc-button');
+    let anyChecked = 0;
+    products.forEach(product => {
+        const checkbox = product.querySelector('.product-item-checkbox');
+        const qtyController = product.querySelector('.quantity-controller');
+        const minusBtn = product.querySelector('.qty-btn.minus');
+        const plusBtn = product.querySelector('.qty-btn.plus');
+        const qtyDisplay = product.querySelector('.qty-display');
+        let quantity = 0;
+        checkbox.addEventListener('change', function() {
+            if (this.checked) {
+                this.parentElement.style.display = 'none';
+                qtyController.style.display = 'flex';
+                quantity = 1;
+                qtyDisplay.textContent = quantity;
+            }
+        });
+        minusBtn.addEventListener('click', function() {
+            quantity = Math.max(0, quantity - 1);
+            qtyDisplay.textContent = quantity;
+            if (quantity === 0) {
+                checkbox.parentElement.style.display = 'block';
+                qtyController.style.display = 'none';
+                checkbox.checked = false;
+                 anyChecked = anyChecked-1;
+                 checkForCalcButton();
+            }
+        });
+        plusBtn.addEventListener('click', function() {
+            quantity++;
+            qtyDisplay.textContent = quantity;
+        });
+        
+    });
+
+    //FUNCIONAMIENTO DE BOTON CALCULAR
+    products.forEach(product => {
+        const checkbox = product.querySelector('.product-item-checkbox');
+        checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    anyChecked = anyChecked+1;
+                    
+                }
+                checkForCalcButton();
+        });
+                
+    });
+
+    checkForCalcButton = () => {
+        if (anyChecked > 0) {
+            calcButton.style.display = 'flex';
+        } else {
+            calcButton.style.display = 'none';
+        }
+    };
 });
